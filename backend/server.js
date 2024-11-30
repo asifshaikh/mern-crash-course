343fac68e4be5a27068944a14ac51b47adbd40f6
asifshaikh <shaikha015@gmail.com>
2024-12-01 01:26:41 +0530

    insert update get api done

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,18 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 
+app.get('/api/products', async (req, res) => {
+  try {
+    const products = await Product.find();
+    res.status(200).json({ success: true, message: products });
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .json({ success: false, message: 'error in getting products' });
+  }
+});
+
 app.post('/api/products', async (req, res) => {
   const product = req.body;
   if (!product.name || !product.price || !product.image) {
@@ -21,6 +33,22 @@ app.post('/api/products', async (req, res) => {
   } catch (error) {
     console.log('Error in creating the product', error.message);
     res.status(500).json({ success: false, message: 'Server Error' });
+  }
+});
+
+app.delete('/api/products/:id', async (req, res) => {
+  const { id } = req.params;
+  console.log(id);
+
+  try {
+    await Product.findByIdAndDelete(id);
+    res
+      .status(400)
+      .json({ success: true, message: 'Product deleted Successfully' });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ success: false, message: 'No matching Product Found' });
   }
 });
 
