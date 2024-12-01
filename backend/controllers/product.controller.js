ad96aa134868e1bcc2d24f51b7c7a88f9795a40c
asifshaikh <shaikha015@gmail.com>
2024-12-02 00:23:15 +0530

    Backend Completed

---
+++ backend/controllers/product.controller.js
@@ -0,0 +1,68 @@
+import Product from '../models/product.model.js';
+import mongoose from 'mongoose';
+export const getProduct = async (req, res) => {
+  try {
+    const products = await Product.find();
+    res.status(200).json({ success: true, message: products });
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .json({ success: false, message: 'error in getting products' });
+  }
+};
+
+export const createProduct = async (req, res) => {
+  const product = req.body;
+  if (!product.name || !product.price || !product.image) {
+    return res
+      .status(400)
+      .json({ success: false, message: 'Please fill in all fields' });
+  }
+  const newProduct = new Product(product);
+  try {
+    await newProduct.save();
+    res.json({ success: true, message: 'Product created successfully' });
+  } catch (error) {
+    console.log('Error in creating the product', error.message);
+    res.status(500).json({ success: false, message: 'Server Error' });
+  }
+};
+
+export const deleteProduct = async (req, res) => {
+  const { id } = req.params;
+  console.log(id);
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res
+      .status(400)
+      .json({ success: false, message: 'Invalid product id' });
+  }
+
+  try {
+    await Product.findByIdAndDelete(id);
+    res
+      .status(400)
+      .json({ success: true, message: 'Product deleted Successfully' });
+  } catch (error) {
+    res.status(500).json({ success: false, message: 'Server Error' });
+  }
+};
+
+export const updateProduct = async (req, res) => {
+  const { id } = req.params;
+  const product = req.body;
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res
+      .status(400)
+      .json({ success: false, message: 'Invalid product id' });
+  }
+  try {
+    const updatedProduct = await Product.findByIdAndUpdate(id, product, {
+      new: true,
+    });
+    res.status(200).json({ success: true, message: updatedProduct });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ success: false, message: 'Server Error' });
+  }
+};
--- backend/server.js
+++ backend/server.js
@@ -1,61 +1,19 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { connectDB } from './config/db.js';
-import Product from './models/product.model.js';
+import productRoutes from './routes/product.route.js';
 dotenv.config();
 
 const app = express();
 app.use(express.json());
+const PORT = process.env.PORT || 5000;
 
-app.get('/api/products', async (req, res) => {
-  try {
-    const products = await Product.find();
-    res.status(200).json({ success: true, message: products });
-  } catch (error) {
-    console.error(error);
-    res
-      .status(500)
-      .json({ success: false, message: 'error in getting products' });
-  }
-});
-
-app.post('/api/products', async (req, res) => {
-  const product = req.body;
-  if (!product.name || !product.price || !product.image) {
-    return res
-      .status(400)
-      .json({ success: false, message: 'Please fill in all fields' });
-  }
-  const newProduct = new Product(product);
-  try {
-    await newProduct.save();
-    res.json({ success: true, message: 'Product created successfully' });
-  } catch (error) {
-    console.log('Error in creating the product', error.message);
-    res.status(500).json({ success: false, message: 'Server Error' });
-  }
-});
-
-app.delete('/api/products/:id', async (req, res) => {
-  const { id } = req.params;
-  console.log(id);
-
-  try {
-    await Product.findByIdAndDelete(id);
-    res
-      .status(400)
-      .json({ success: true, message: 'Product deleted Successfully' });
-  } catch (error) {
-    res
-      .status(500)
-      .json({ success: false, message: 'No matching Product Found' });
-  }
-});
+app.use('/api/products', productRoutes);
 
 console.log(process.env.MONGO_URI);
 
-app.listen(5000, () => {
+app.listen(PORT, () => {
   connectDB();
-  console.log('server is running on port 5000');
+  console.log('server is running on port:' + PORT);
 });
 //
