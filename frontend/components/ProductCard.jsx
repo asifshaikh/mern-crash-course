005639edf1fb08856599c4b6af2423b66e5485b5
asifshaikh <shaikha015@gmail.com>
2024-12-06 20:50:18 +0530

    Delete done

---
+++ frontend/components/ProductCard.jsx
@@ -0,0 +1,78 @@
+/* eslint-disable react/prop-types */
+import {
+  Box,
+  Heading,
+  HStack,
+  IconButton,
+  Image,
+  Text,
+  useColorModeValue,
+  useToast,
+} from '@chakra-ui/react';
+import { DeleteIcon, EditIcon } from '@chakra-ui/icons';
+import { useProductStore } from '../src/store/Product';
+
+const ProductCard = ({ product }) => {
+  const textColor = useColorModeValue('gray.600', 'gray.200');
+  const bg = useColorModeValue('white', 'gray.800');
+
+  const toast = useToast();
+  const { deleteProduct } = useProductStore();
+  const handleDeleteProduct = async (pid) => {
+    const { success, message } = await deleteProduct(pid);
+    if (!success) {
+      toast({
+        title: 'Error',
+        description: message,
+        status: 'error',
+        duration: 3000,
+        isClosable: true,
+      });
+    } else {
+      toast({
+        title: 'Success',
+        description: message,
+        status: 'success',
+        duration: 3000,
+        isClosable: true,
+      });
+    }
+  };
+  return (
+    <Box
+      shadow='lg'
+      rounded='lg'
+      overlay='hidden'
+      transition='all 0.3s'
+      _hover={{ transform: 'translateY(-5px)', shadow: 'xl' }}
+      bg={bg}
+    >
+      <Image
+        src={product.image}
+        alt={product.name}
+        h={48}
+        w='full'
+        objectFit='cover'
+      />
+      <Box p={5}>
+        <Heading as={'h3'} size='md' mb={2}>
+          {product.name}
+        </Heading>
+        <Text fontWeight={'bold'} fontSize='xl' color={textColor} mb={4}>
+          ${product.price}
+        </Text>
+
+        <HStack spacing={4}>
+          <IconButton icon={<EditIcon />} colorScheme='blue' />
+          <IconButton
+            icon={<DeleteIcon />}
+            colorScheme='red'
+            onClick={() => handleDeleteProduct(product._id)}
+          />
+        </HStack>
+      </Box>
+    </Box>
+  );
+};
+
+export default ProductCard;
--- frontend/pages/HomePage.jsx
+++ frontend/pages/HomePage.jsx
@@ -1,5 +1,67 @@
+import { Container, SimpleGrid, Text, VStack } from '@chakra-ui/react';
+import { Link } from 'react-router-dom';
+import { useProductStore } from '../src/store/Product';
+import { useEffect } from 'react';
+import ProductCard from '../components/ProductCard';
+
 const HomePage = () => {
-  return <div>HomePage</div>;
+  const { fetchProduct, products } = useProductStore();
+
+  useEffect(() => {
+    fetchProduct();
+  }, [fetchProduct]);
+  console.log(products);
+  return (
+    <Container maxW={'container.lg'} py={12}>
+      <VStack spacing={4}>
+        <Text
+          fontSize={30}
+          fontWeight={'bold'}
+          bgGradient={[
+            'linear(to-tr, teal.300, yellow.400)',
+            'linear(to-t, blue.200, teal.500)',
+            'linear(to-b, orange.100, purple.300)',
+          ]}
+          bgClip='text'
+          textAlign={'center'}
+        >
+          Current Products
+        </Text>
+        <SimpleGrid
+          columns={{
+            base: 1,
+            md: 2,
+            lg: 3,
+          }}
+          spacing={8}
+        >
+          {products.map((product) => (
+            <ProductCard key={product._id} product={product} />
+          ))}
+        </SimpleGrid>
+
+        {products.length === 0 && (
+          <Text
+            fontSize={'xl'}
+            textAlign={'center'}
+            fontWeight={'bold'}
+            color={'gray'}
+          >
+            No Products Found{' '}
+            <Link to={'/create'}>
+              <Text
+                as={'span'}
+                color='blue.500'
+                _hover={{ textDecoration: 'underline' }}
+              >
+                Create a Product
+              </Text>
+            </Link>
+          </Text>
+        )}
+      </VStack>
+    </Container>
+  );
 };
 
 export default HomePage;
